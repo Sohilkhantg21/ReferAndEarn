6936d87398397cb1ddc0a5163a2835c681c043f7
Sohil [SCRIPTER🔥] <pathansohilkhan2005@gmail.com>
2025-04-22 21:19:03 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+define('API_KEY', '7544666074:AAHqFGyXK4r08Bz9D50_8uOlDgWJnl0dTxE'); //Add Bot token here
+
+$per_refer=3; //add per refer amount
+
+$min_withdraw=20; //add minimum Withdraw amount
+
+$max_withdraw=30; //add maximum Withdraw amount
+$channel="@sohilscripter"; //add your channel
+$mid=""; //Enter Your Full2Sms MID
+
+$mkey=""; //Enter Your Full2ms MKEY
+
+$guid=""; //Enter Your Full2sms GUID
+
+$comment = ""; //Enter The Payment Comment
+?>
